58424b66f6705ee015198540bc545902a0c8bab8
0350tnerghes <0350tnerghes@e-itaca.es>
2022-04-27 11:05:50 +0200

    primer commit

---
+++ src/test/java/com/hiberusUniversity/tamara/maven/first/AppTest.java
@@ -0,0 +1,20 @@
+package com.hiberusUniversity.tamara.maven.first;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
